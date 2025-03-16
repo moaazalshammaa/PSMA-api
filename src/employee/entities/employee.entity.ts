bcbc7d5bb483ef5e19d3cb0ef7bbc02096af20cf
MoazSh <moazalshamma23@gmail.com>
2025-03-16 14:59:49 +0300

    set up authentication files

--- src/employee/entities/employee.entity.ts
+++ src/employee/entities/employee.entity.ts
@@ -1,6 +1,16 @@
-import { Store } from "src/stores/entities/stores.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-
+import { Store } from 'src/stores/entities/stores.entity';
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+export enum UserRole {
+  ADMIN = 'admin',
+  EMPLOYEE = 'employee',
+}
 @Entity()
 export class Employee {
   @PrimaryGeneratedColumn()
@@ -9,8 +19,24 @@ export class Employee {
   @Column()
   name: string;
 
-  
+  @Column({ unique: true })
+  email: string;
 
-  @ManyToOne(() => Store, store => store.employees)
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.EMPLOYEE,
+  })
+  role: UserRole;
+
+  @ManyToOne(() => Store, (store) => store.employees)
   store: Store;
 }
---
+++ src/employee/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { Employee } from '../entities/employee.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(Employee)
+    private readonly userRepository: Repository<Employee>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async register(email: string, password: string) {
+    const newUser = this.userRepository.create({ email, password });
+    await this.userRepository.save(newUser);
+    return { message: 'User registered successfully' };
+  }
+}
